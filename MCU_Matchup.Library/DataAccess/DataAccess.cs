734cc5dc1d2b297e39f918bf558f42ca4a36641d
Brian <brianj.pajak@gmail.com>
2020-07-21 15:43:22 -0400

    Add project files.

---
+++ MCU_Matchup.Library/DataAccess/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCU_Matchup.Library.DataAccess
+{
+
+    //using RestSharp;
+    //using RestSharp.Authenticators;
+
+    //var client = new RestClient("https://api.twitter.com/1.1");
+    //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+    //var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+
+    //var response = client.Get(request);
+    public class DataAccess
+    {
+
+        public void GetCharacter(string name)
+        {
+            var client = new RestClient("https://imdb8.p.rapidapi.com/title/get-charname-list?currentCountry=US&marketplace=ATVPDKIKX0DER&purchaseCountry=US&id=nm3964231&tconst=tt0944947");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "imdb8.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "3acacf8feemsh8281560185a0955p1e8594jsn954f4395d7b5");
+            IRestResponse response = client.Execute(request);
+
+        }
+    }
+}
